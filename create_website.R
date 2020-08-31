c8f87aa806f4142d2a543a0a3ec496236b09b1a8
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-09-01 01:13:35 +0200

    Update 4.

--- create_website.R
+++ create_website.R
@@ -7,4 +7,6 @@ blogdown::serve_site()
 blogdown::stop_server()
 # blogdown::new_post("Welcome to collapse")
 
+# replace R, Econometrics, High Performance - R, Econometrics, High Performance with one time only
+
 # https://www.r-bloggers.com/start-blogging-in-5-minutes-on-netlify-with-hugo-and-blogdown-september-2019-update/
