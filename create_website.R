63bb215768124f927253a7d6c84d515a9849f4f2
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-08-31 18:27:00 +0200

    Update.

--- create_website.R
+++ create_website.R
@@ -1,9 +1,10 @@
 library(blogdown)
 blogdown::install_hugo()
 # setwd("website")
-blogdown::new_site() # theme = 'gcushen/hugo-academic'
+# blogdown::new_site() # theme = 'gcushen/hugo-academic'
+blogdown::build_site()
 blogdown::serve_site()
 blogdown::stop_server()
-blogdown::new_post("Welcome to collapse")
+# blogdown::new_post("Welcome to collapse")
 
 # https://www.r-bloggers.com/start-blogging-in-5-minutes-on-netlify-with-hugo-and-blogdown-september-2019-update/
