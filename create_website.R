265c7d4a5d0de11d71bbb3ce99ba40fa41be92a4
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2020-09-13 06:04:02 +0200

    Updating RSS to exclude 'about' page. Also updating introduction post a little bit.

--- create_website.R
+++ create_website.R
@@ -8,6 +8,11 @@ blogdown::stop_server()
 # blogdown::new_post("Welcome to collapse")
 
 # <description>{{ .Content | html }}</description>
+# <description>{{ "<![CDATA[" | safeHTML }} {{ .Content }}]]></description>
+
+# https://benjamincongdon.me/blog/2020/01/14/Tips-for-Customizing-Hugo-RSS-Feeds/
+# REplace  {{ range $pages }} (line 28) by 
+# {{ range where (where .Site.Pages ".Section" "post") "Kind" "page" }}
 
 # replace R, Econometrics, High Performance - R, Econometrics, High Performance with one time only
 
